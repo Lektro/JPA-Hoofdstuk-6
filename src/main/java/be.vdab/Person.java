21c51f5e82e80d7cdad050190a6cf3dbcab59d9a
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-09-30 11:27:15 +0200

    started on person class

--- src/main/java/be.vdab/Person.java
+++ src/main/java/be.vdab/Person.java
@@ -1,11 +1,14 @@
 package be.vdab;
 
-import javax.persistence.Basic;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 @Entity
+@Table (name ="PERSONS", uniqueConstraints = {@UniqueConstraint(columnNames ="id")})
 public class Person {
 
     @Id
@@ -13,15 +16,113 @@ public class Person {
     private long version;
     private String firstName;
     private String lastName;
+    private int age;
     private LocalDate birthDay;
     private GenderType gender;
     private byte[] picture;
     private String comment;
-
-    @Basic(optional = false)
     private boolean married;
-
-    private int age;
     private String homepage;
 
+
+    // Getters and Setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Transient
+    public int getAge() {
+        LocalDate now = LocalDate.now();
+        int age = Math.toIntExact(ChronoUnit.YEARS.between(now, birthDay));
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Id
+    @Enumerated(EnumType.STRING)
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
+
+    @Lob
+    @Column(nullable = true)
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Basic(optional = false)
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
 }
